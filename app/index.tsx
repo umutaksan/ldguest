986d0d84929f5ba5638ea8ca0ff3d402b7176be7
umutaksan <139861552+umutaksan@users.noreply.github.com>
2025-07-07 23:07:36 +0300

    Updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -133,7 +133,6 @@ export default function PropertiesScreen() {
               <Text style={styles.footerLink}>hello@ldguest.com</Text>
             </TouchableOpacity>
           </View>
-          </View>
         </ScrollView>
       </ResponsiveContainer>
     </View>
