48dafbaa2c37c0143c5a0dd30d6cb5f2b4ab8b6e
umutaksan <139861552+umutaksan@users.noreply.github.com>
2026-01-16 00:56:05 +0100

    Added +html.tsx

---
+++ app/+html.tsx
@@ -0,0 +1,53 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import { type PropsWithChildren } from 'react';
+
+/**
+ * This file is web-only and used to configure the root HTML for every web page during static rendering.
+ * The contents of this function only run in Node.js environments and do not have access to the DOM or browser APIs.
+ */
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, user-scalable=no"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#E9B872" />
+
+        <title>L&D Guest Marbella</title>
+        <meta name="description" content="Welcome to L&D Guest Marbella - Your perfect holiday rental in Marbella" />
+
+        {/* Prevents text size adjustment on orientation change - important for iPad */}
+        <style dangerouslySetInnerHTML={{
+          __html: `
+            * {
+              -webkit-text-size-adjust: 100%;
+              text-size-adjust: 100%;
+            }
+            html, body, #root {
+              width: 100%;
+              height: 100%;
+              margin: 0;
+              padding: 0;
+              overflow-x: hidden;
+            }
+            body {
+              font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+              -webkit-font-smoothing: antialiased;
+              -moz-osx-font-smoothing: grayscale;
+            }
+          `
+        }} />
+
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
