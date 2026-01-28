f7666cccde3a65b75d500e7bd53d1bc89bd160c5
umutaksan <139861552+umutaksan@users.noreply.github.com>
2026-01-28 17:00:22 +0300

    Updated package-lock.json

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,15 +1,1 @@
-declare module '@env' {
-  export const EXPO_PUBLIC_SUPABASE_URL: string;
-  export const EXPO_PUBLIC_SUPABASE_ANON_KEY: string;
-}
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      EXPO_PUBLIC_SUPABASE_URL: string;
-      EXPO_PUBLIC_SUPABASE_ANON_KEY: string;
-    }
-  }
-}
-
 export {};
